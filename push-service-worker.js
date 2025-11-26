9d090c6dbe3d2c84a6e6d25f0dc6919320b31dfb
ghana <binancenft32@gmail.com>
2025-11-26 18:24:58 +0300

    Update push-service-worker.js

--- push-service-worker.js
+++ push-service-worker.js
@@ -1,6 +1,1 @@
-// push-service-worker.js
-
-// Это файл Service Worker, который будет использовать OneSignal.
-// Он должен быть максимально чистым для корректной работы SDK.
-
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
